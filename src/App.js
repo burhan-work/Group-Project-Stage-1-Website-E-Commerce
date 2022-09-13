d43533ff8f4dd241851901ef9025423922f63205
FebbyLimasSendelvant <febbylimas98@gnail.com>
2022-09-13 22:34:44 +0800

    contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import "./App.css";
 import Product from "./pages/Product";
 import Login from './pages/Login';
+import Contact from './pages/Contact';
 import {Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         {/* <Route path='/' element={<Home/>} /> */}
         {/* <Route path='/about' element={<About/>} /> */}
         <Route path='/product' element={<Product/>} />
+        <Route path='/contact' element={<Contact/>} />
         {/* <Route path='/cart' element={<Cart/>} /> */}
       </Routes>
     </>
