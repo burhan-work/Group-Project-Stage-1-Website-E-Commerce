1f9d44918c147fa069f49402d019f734a9e4d279
burhan-work <burhan.bnw@gmail.com>
2022-09-13 23:04:05 +0800

    update structure navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Product from "./pages/Product";
 import Login from './pages/Login';
+import Cart from './pages/Cart';
 import Contact from './pages/Contact';
 import Register from "./pages/Register";
 import {Routes, Route} from 'react-router-dom';
@@ -11,11 +15,11 @@ function App() {
     <>      
       <Navbar />
       <Routes>
-        {/* <Route path='/' element={<Home/>} /> */}
-        {/* <Route path='/about' element={<About/>} /> */}
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
         <Route path='/product' element={<Product/>} />
         <Route path='/contact' element={<Contact/>} />
-        {/* <Route path='/cart' element={<Cart/>} /> */}
+        <Route path='/cart' element={<Cart/>} />
         <Route path='/Login' element={<Login />} />
         <Route path='/Register' element={<Register />} />
       </Routes>
